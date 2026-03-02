7d4e666efa5277041fd26ec62dcd6b0ec48306e2
kaviyarasan033 <mkaviyarasan003@gmail.com>
2026-03-02 09:57:35 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,37 @@ const themes = [
 
   { green: "#00C853", yellow: "#FFD600" },
 
-
   { green: "#7B1FA2", yellow: "#F48FB1" },
-
 
   { green: "#D32F2F", yellow: "#FFCC80" },
 
-
   { green: "#1976D2", yellow: "#81D4FA" },
+
+
+  { green: "#2E7D32", yellow: "#FFF176" },
+
+
+  { green: "#00897B", yellow: "#80DEEA" },
+
+
+  { green: "#E65100", yellow: "#FFAB91" },
+
+
+  { green: "#C2185B", yellow: "#E1BEE7" },
+
+
+  { green: "#303F9F", yellow: "#B3E5FC" },
+
+  { green: "#AFB42B", yellow: "#B9F6CA" },
+
+ 
+  { green: "#5D4037", yellow: "#D7CCC8" },
+
+
+  { green: "#00E676", yellow: "#FFEA00" },
+
+
+  { green: "#263238", yellow: "#FFC107" },
 ];
 
 
